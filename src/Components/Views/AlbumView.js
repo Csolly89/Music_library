d5c33fb6e8c3e3364fce732bd53fb8dde48d3fec
Csolly89 <141281525+Csolly89@users.noreply.github.com>
2023-11-29 19:41:21 -0500

    albumview complete

--- src/Components/Views/AlbumView.js
+++ src/Components/Views/AlbumView.js
@@ -4,13 +4,34 @@ import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
 function AlbumView() {
-    const [ albumData, setAlbumtData ] = useState([])
+    const [ albumData, setAlbumData ] = useState([])
 
     const { id } = useParams()
+
+    useEffect (() => {
+        const fetchData = async () => {
+            const url = `http://localhost:4000/song/${id}`
+            const response = await fetch(url)
+            const data = await response.json()
+
+            const songs = data.results.filter(item => item.wrapperType === 'track')
+            setAlbumData(songs)
+        }
+        fetchData()
+    }, [id])
+
+    const songDisplay = albumData.map(song => {
+        return (
+            < div key={song.trackId}>
+                <p>{song.trackName}</p>
+            </div>
+        )
+    })
     return (
         <div>
             <p>Album Data Goes Here!</p>
             <p>ID: {id}</p>
+            {songDisplay}
         </div>
     )
 }
